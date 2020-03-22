39e6468aa72da23bf01665b1bfdffd16a506171e
D3VBG <59423787+D3VBG@users.noreply.github.com>
2020-03-22 19:10:24 +0200

    fixes

--- KodBlog1/urls.py
+++ KodBlog1/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
+admin.site.site_header = "KodAdmin"
+admin.site.site_title = "Admin Panel"
+admin.site.index_title = "Welcome to KodAdmin Panel"
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
